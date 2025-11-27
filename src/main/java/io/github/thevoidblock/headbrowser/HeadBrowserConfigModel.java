bdc9f86063e479d18dfd2fafbabe170fde9a927c
void <wordhh1@gmail.com>
2025-11-26 19:13:37 -0500

    Add a config option to disable tooltip hints

--- src/main/java/io/github/thevoidblock/headbrowser/HeadBrowserConfigModel.java
+++ src/main/java/io/github/thevoidblock/headbrowser/HeadBrowserConfigModel.java
@@ -15,6 +15,7 @@ public class HeadBrowserConfigModel {
     public boolean titleButton = true;
     public boolean pauseButton = true;
     public boolean autoQuery = false;
+    public boolean tooltipHints = true;
 
     @SectionHeader("advanced")
     public long cacheExpirationTime = 86400;
--- src/main/java/io/github/thevoidblock/headbrowser/Styler.java
+++ src/main/java/io/github/thevoidblock/headbrowser/Styler.java
@@ -7,6 +7,7 @@ import net.minecraft.util.Formatting;
 import net.minecraft.world.GameMode;
 
 import static io.github.thevoidblock.headbrowser.HeadBrowser.CLIENT;
+import static io.github.thevoidblock.headbrowser.HeadBrowser.CONFIG;
 import static io.github.thevoidblock.headbrowser.MinecraftHeadsAPI.HEADS;
 
 public class Styler {
@@ -14,25 +15,29 @@ public class Styler {
         MutableText nameText = (MutableText) Text.of(name);
         nameText.setStyle(Style.EMPTY.withColor(Formatting.YELLOW).withBold(true));
 
-        MutableText infoText = Text.empty().setStyle(Style.EMPTY.withBold(false).withColor(Formatting.WHITE));
+        MutableText hints = Text.empty().setStyle(Style.EMPTY.withBold(false).withColor(Formatting.WHITE));
 
         if(CLIENT.player != null && CLIENT.player.getGameMode() == GameMode.CREATIVE) {
-            infoText.append(Text.translatable("tooltip.headbrowser.head.left-click-option", Text.translatable("tooltip.headbrowser.head.get-head-option")));
-            infoText.append("\n");
+            hints.append(Text.translatable("tooltip.headbrowser.head.left-click-option", Text.translatable("tooltip.headbrowser.head.get-head-option")));
+            hints.append("\n");
         }
 
-        infoText.append(Text.translatable("tooltip.headbrowser.head.middle-click-option", Text.translatable("tooltip.headbrowser.head.equip-as-skin-option")));
+        hints.append(Text.translatable("tooltip.headbrowser.head.middle-click-option", Text.translatable("tooltip.headbrowser.head.equip-as-skin-option")));
 
-        infoText.append("\n");
-        infoText.append(Text.translatable("tooltip.headbrowser.head.right-click-option", Text.translatable("tooltip.headbrowser.head.view-head-info-option")));
+        hints.append("\n");
+        hints.append(Text.translatable("tooltip.headbrowser.head.right-click-option", Text.translatable("tooltip.headbrowser.head.view-head-info-option")));
 
         MutableText categoryInfoText = (MutableText) HEADS.getCategoryText(categoryId);
         categoryInfoText.setStyle(Style.EMPTY.withBold(false).withColor(Formatting.DARK_GRAY));
 
         MutableText tooltipText = Text.empty();
         tooltipText.append(nameText);
-        tooltipText.append("\n");
-        tooltipText.append(infoText);
+
+        if(CONFIG.tooltipHints()) {
+            tooltipText.append("\n");
+            tooltipText.append(hints);
+        }
+
         tooltipText.append("\n");
         tooltipText.append(categoryInfoText);
 
